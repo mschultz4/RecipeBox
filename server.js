f00db02675169e3d36bac3463848da7ace657b93
Amin Meyghani <meyghania@gmail.com>
2016-09-24 15:01:54 -0700

    my backend example

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 var express = require('express');
+
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+
+
+
+var bodyParser = require('body-parser');
 var app = express();
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 var port = process.env.PORT || 5000;
 
 app.use(express.static('./dist'));
@@ -8,6 +19,31 @@ app.get('/', function (req, res) {
   res.send('hello');
 });
 
+app.get('/api/data', function (req, res) {
+  return res.json({
+    data: 'hello there'
+  });
+});
+
+
+app.post('/api/signup', function (req, res) {
+
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+      console.log(hash);
+      var hashPassword = hash;
+      var email = req.body.email;
+      var user = {
+        email: email,
+        password: hashPassword
+      };
+      return res.json({
+        message: "user created",
+        data: user
+      });
+    });
+  })
+});
 
 app.listen(port, function () {
   console.log('Example app listening on port 5000!');
